f20324780a25a22cb9db0202d706c4feb68a6e41
Ad-rian27 <rian270627@gmail.com>
2026-07-15 22:23:49 +0530

    SignIn api completed

--- app.js
+++ app.js
@@ -11,7 +11,55 @@ app.use(Express.json())
 app.use(Cors())
 Mongoose.connect("mongodb+srv://adrian:adrian123@cluster0.veegpvo.mongodb.net/blogapp?appName=Cluster0")
 
-app.get("/signup", async (req, res) => {
+
+//Sign In
+app.post("/signin", async (req, res) => {
+
+    let input = req.body
+
+    let result = userModel.find({ email: req.body.email }).then(
+        (items) => {
+            if (items.length > 0) {
+
+                const passwordValidator = Bcrypt.compareSync(
+                    req.body.password,
+                    items[0].password
+                )
+
+                if (passwordValidator) {
+                    jwt.sign(
+                        { email: req.body.email },
+                        "blogApp",
+                        { expiresIn: "1d" },
+                        (error, token) => {
+                            if (error) {
+                                res.json({
+                                    "status": "error",
+                                    "errorMessage": error
+                                })
+                            }
+                            else {
+res.json({ "status": "success", "token":token, "userId":items[0]._id })
+                            }
+                        }
+                    )
+                } else {
+                    res.json({ "status": "Incorrect Password"})
+                }
+
+            } else {
+                res.json({ "status": "Invalid Email Id" })
+            }
+        }
+    ).catch()
+
+})
+
+
+
+
+//Sign up
+app.post("/signup", async (req, res) => {
     let input = req.body
     let hashedPassword = Bcrypt.hashSync(req.body.password, 10)
     console.log(hashedPassword)
