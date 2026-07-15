27d72106c93bc6d2df780573fc079935d7fc1efa
Ad-rian27 <rian270627@gmail.com>
2026-07-15 21:52:06 +0530

    Signup api complete

--- app.js
+++ app.js
@@ -3,12 +3,40 @@ const Mongoose = require("mongoose")
 const Bcrypt = require("bcrypt")
 const Cors = require("cors")
 const jwt = require("jsonwebtoken")
+const userModel = require("./models/users")
 
 let app = Express()
 
-app.get("/", (req, res) => {
-    res.send("hello")
-})
+app.use(Express.json())
+app.use(Cors())
+Mongoose.connect("mongodb+srv://adrian:adrian123@cluster0.veegpvo.mongodb.net/blogapp?appName=Cluster0")
+
+app.get("/signup", async (req, res) => {
+    let input = req.body
+    let hashedPassword = Bcrypt.hashSync(req.body.password, 10)
+    console.log(hashedPassword)
+    req.body.password = hashedPassword
+    console.log(input)
+
+    userModel.find({ email: req.body.email }).then(
+        (items) => {
+            if (items.length > 0) {
+                res.json({ "status": "email Id already exists" })
+            } else {
+                let result = new userModel(input)
+                result.save()
+                res.json({ "status": "success" })
+            }
+        }
+    ).catch(
+        (error) => { }
+    )
+
+
+}
+
+
+)
 
 app.listen(3000, () => {
     console.log("Server started")
